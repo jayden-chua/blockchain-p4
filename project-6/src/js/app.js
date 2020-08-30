301b063a9c2ec37a4306dc7468431ce64d9fdacf
Jayden Chua <gjiehs@gmail.com>
2020-08-30 12:55:51 +0800

    change deployments to rinkeby, install hd-wallet

--- project-6/src/js/app.js
+++ project-6/src/js/app.js
@@ -69,13 +69,13 @@ App = {
             }
         }
         // Legacy dapp browsers...
-        else if (window.web3) {
-            App.web3Provider = window.web3.currentProvider;
-        }
+        // else if (window.web3) {
+        //     App.web3Provider = window.web3.currentProvider;
+        // }
         // If no injected web3 instance is detected, fall back to Ganache
-        else {
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-        }
+        // else {
+        //     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        // }
 
         App.getMetaskAccountID();
 
--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,18 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "f12d737a380c41d494508dcc37254eff";
+const mnemonic = "language tragic drift envelope damp vapor tube rotate prevent attend use nature";
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
     }
   },
   compilers: { solc: { version: "0.4.24" } }
